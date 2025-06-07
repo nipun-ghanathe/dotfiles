cc13b8c4dda608ea7c743cd2ba27fc4101306b21
nipun <nipuncod@gmail.com>
2025-06-07 21:47:53 +0530

    Added nodejs setup in zsh configs

--- zsh/.config/zsh/general.zsh
+++ zsh/.config/zsh/general.zsh
@@ -40,3 +40,8 @@ eval "$(zoxide init --cmd cd zsh)"
 # if [ -z "$TMUX" ]; then
 #   tmux -A -s main
 # fi
+
+# node.js
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
