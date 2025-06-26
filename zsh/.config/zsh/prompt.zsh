6b70a9dba78a4bea73b40240c1fc798ac4c88371
nipun <nipuncod@gmail.com>
2025-06-26 20:15:35 +0530

    kitty padding removed from not used sides, added comments to zsh prompt

--- zsh/.config/zsh/prompt.zsh
+++ zsh/.config/zsh/prompt.zsh
@@ -1,5 +1,7 @@
+# Evaluate variables and command substitutions inside prompt string
 setopt prompt_subst
 
+# Git Branch for prompt
 git_branch() {
   local branch dirty # remote ahead behind remote_status
   # Branch check
@@ -12,8 +14,8 @@ git_branch() {
   fi
 }
 
+# Configuring Virtual Env and Conda for Prompt
 export VIRTUAL_ENV_DISABLE_PROMPT=1
-
 get_env_name() {
   if [[ -n "$CONDA_DEFAULT_ENV" ]]; then
     echo " via %F{green}🅒 $CONDA_DEFAULT_ENV%f"
@@ -22,18 +24,20 @@ get_env_name() {
   fi
 }
 
+# Before displaying prompt
 precmd() {
   if [[ $? -eq 0 ]]; then
-    prompt_symbol="%B%F{green}➜%f%b"
+    prompt_symbol="%B%F{green}➜%f%b "
   else
-    prompt_symbol="%B%F{red}✗%f%b"
+    prompt_symbol="%B%F{red}✗%f%b "
   fi
 }
 
+# Setting different prompts
 PROMPT=$'\n'
 PROMPT+='%B%F{cyan}%1~%f%b'
 PROMPT+='$(git_branch)'
 PROMPT+='$(get_env_name)'
-PROMPT+=$'\n${prompt_symbol} ' 
+PROMPT+=$'\n${prompt_symbol}' 
 
 PS2='%F{green}›%f ' 
