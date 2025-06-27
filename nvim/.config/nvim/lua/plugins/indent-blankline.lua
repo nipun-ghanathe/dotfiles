772ad956d65823c3152bb54a5038d22cd970e7fc
nipun <nipuncod@gmail.com>
2025-06-27 23:28:58 +0530

    removed useless things from neovim

--- nvim/.config/nvim/lua/plugins/indent-blankline.lua
+++
@@ -1,9 +0,0 @@
----@diagnostic disable: undefined-doc-name
-
-return {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    ---@module "ibl"
-    ---@type ibl.config
-    opts = {},
-}
