425a34053ac0e33f0a2e0967f4d90a062aecef68
nipun <nipuncod@gmail.com>
2025-07-02 16:33:17 +0530

    separated wallpapers for dracula and nord

--- sway/.config/sway/scripts/random_wallpaper.sh
+++ sway/.config/sway/scripts/random_wallpaper.sh
@@ -2,7 +2,8 @@
 
 pkill swaybg
 
-WALLPAPER_DIR="$HOME/dotfiles/wallpapers"
+# WALLPAPER_DIR="$HOME/dotfiles/wallpapers"
+WALLPAPER_DIR="$HOME/dotfiles/wallpapers/nord"
 
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f \( -iname '*.jpg' -o -iname '*.png' -o -iname '*.jpeg' \) | shuf -n 1)
 
