90375b89300a029989da2dd6b4e9f2034f3b133c
nipun <nipuncod@gmail.com>
2025-07-20 00:05:17 +0530

    Added user commands to toggle format on save feature in conform.nvim

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -2,10 +2,15 @@ return {
   "stevearc/conform.nvim",
   config = function()
     require("conform").setup({
-      format_on_save = {
-        timeout_ms = 500,
+      default_format_opts = {
         lsp_format = "fallback",
       },
+      format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_format = "fallback" }
+      end,
       formatters_by_ft = {
         lua = { "stylua" },
         python = {
@@ -16,6 +21,25 @@ return {
         html = { "djlint" },
         markdown = { "mdformat" },
       },
+    })
+
+    -- User commands for toggling autoformat-on-save
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = "Disable autoformat-on-save",
+      bang = true,
+    })
+    vim.api.nvim_create_user_command("FormatEnable", function(args)
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = "Re-enable autoformat-on-save",
     })
 
     vim.keymap.set("n", "<leader>fc", function()
