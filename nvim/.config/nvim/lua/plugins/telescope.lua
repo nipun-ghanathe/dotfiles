6524c8e4a81a4a6479cf55d3b6a9dcac8212014c
nipun <nipuncod@gmail.com>
2025-07-22 23:10:21 +0530

    Added telescope-fzf-native to neovim

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,6 +2,10 @@ return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+    },
     "nvim-telescope/telescope-ui-select.nvim",
   },
   config = function()
@@ -29,11 +33,18 @@ return {
         },
       },
       extensions = {
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = "smart_case",
+        },
         ["ui-select"] = {
           require("telescope.themes").get_dropdown(),
         },
       },
     })
+    require("telescope").load_extension("fzf")
     require("telescope").load_extension("ui-select")
 
     local builtin = require("telescope.builtin")
