abea38217fae0501cc0701378ec204b56cd623b1
nipun <nipuncod@gmail.com>
2025-08-10 21:23:50 +0530

    Configured Luasnip for custom snippets using lua

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -4,25 +4,42 @@ return {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
-    "rafamadriz/friendly-snippets",
+    {
+      "L3MON4D3/LuaSnip",
+      version = "*",
+      dependencies = {
+        "rafamadriz/friendly-snippets",
+        "saadparwaiz1/cmp_luasnip",
+      },
+      config = function()
+        -- Every unspecified option will be set to default.
+        require("luasnip").config.set_config({
+          history = true,
+          updateevents = "TextChanged,TextChangedI",
+          ext_opts = {
+            [require("luasnip.util.types").choiceNode] = {
+              active = {
+                virt_text = { { "choiceNode", "Comment" } },
+              },
+            },
+          },
+        })
+
+        -- snippets from rtp/<folder-with-package.json> (friendly-snippets)
+        require("luasnip.loaders.from_vscode").lazy_load()
+        require("luasnip").filetype_extend("javascript", { "jsdoc" })
+        require("luasnip").filetype_extend("typescript", { "tsdoc" })
+
+        -- snippets from rtp/luasnippets (my-custom-snippets)
+        require("luasnip.loaders.from_lua").lazy_load()
+      end,
+    },
   },
   event = "InsertEnter",
   config = function()
     local cmp = require("cmp")
 
-    -- load the extra snippets from friednly-snippets
-    require("luasnip.loaders.from_vscode").lazy_load()
-    require("luasnip").filetype_extend("javascript", { "jsdoc" })
-    require("luasnip").filetype_extend("typescript", { "tsdoc" })
-
     cmp.setup({
-      snippet = {
-        expand = function(args)
-          require("luasnip").lsp_expand(args.body)
-        end,
-      },
       sources = cmp.config.sources({
         { name = "nvim_lsp", keyword_length = 2 },
         { name = "luasnip" },
@@ -32,6 +49,27 @@ return {
       preselect = "item",
       completion = {
         completeopt = "menu,menuone,noinsert",
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+      formatting = {
+        format = function(entry, vim_item)
+          -- Source
+          vim_item.menu = ({
+            nvim_lsp = "[lsp]",
+            luasnip = "[luasnip]",
+            buffer = "[buffer]",
+            path = "[path]",
+          })[entry.source.name]
+          return vim_item
+        end,
+      },
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
       },
       mapping = cmp.mapping.preset.insert({
         ["<c-y>"] = cmp.mapping.confirm({ select = true }), -- confirm completion item
@@ -64,23 +102,27 @@ return {
             fallback()
           end
         end, { "i", "s" }),
+
+        -- cycle forward through choice nodes
+        ["<c-j>"] = cmp.mapping(function(fallback)
+          local luasnip = require("luasnip")
+          if luasnip.choice_active() then
+            luasnip.change_choice(1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+
+        -- cycle backward through choice nodes
+        ["<c-k>"] = cmp.mapping(function(fallback)
+          local luasnip = require("luasnip")
+          if luasnip.choice_active() then
+            luasnip.change_choice(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
       }),
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      },
-      formatting = {
-        format = function(entry, vim_item)
-          -- Source
-          vim_item.menu = ({
-            nvim_lsp = "[lsp]",
-            luasnip = "[luasnip]",
-            buffer = "[buffer]",
-            path = "[path]",
-          })[entry.source.name]
-          return vim_item
-        end,
-      },
     })
   end,
 }
