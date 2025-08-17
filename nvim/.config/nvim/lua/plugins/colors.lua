e02d12045e92475cc07eecb7d835211f1bc1812d
nipun <nipuncod@gmail.com>
2025-08-17 11:44:32 +0530

    More setup for colorscheme

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -3,7 +3,28 @@ return {
   name = "catppuccin",
   lazy = false,
   priority = 1000,
-  config = function()
-    vim.cmd.colorscheme("catppuccin-macchiato")
+  opts = {
+    flavour = "macchiato",
+    background = {
+      light = "latte",
+      dark = "macchiato",
+    },
+    -- don't use following for transparency
+    -- it disrupts other elements (like lualine)
+    -- transparent_background = true,
+    auto_integrations = true,
+  },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
+
+    vim.cmd.colorscheme("catppuccin")
+
+    -- for transparency
+    -- vim.cmd([[
+    --   hi Normal guibg=NONE ctermbg=NONE
+    --   hi NormalNC guibg=NONE ctermbg=NONE
+    --   hi SignColumn guibg=NONE
+    --   hi EndOfBuffer guibg=NONE
+    -- ]])
   end,
 }
