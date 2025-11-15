575d00b901dbd7e1ddeeb045356f35339aa86ba2
nipun <nipuncod@gmail.com>
2025-11-15 15:51:50 +0530

    Configured obsidian.nvim

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -5,18 +5,32 @@ return {
   event = { "BufReadPre " .. vim.fn.expand("~/obsidian-vault/") },
   -- Replace the above line with `ft = "markdown"` if you want to load obsidian.nvim for all markdown files
   dependencies = { "nvim-lua/plenary.nvim" },
-  -- opts = {
-  --   workspaces = {
-  --     {
-  --       name = "personal",
-  --       path = "~/vaults/personal",
-  --     },
-  --     {
-  --       name = "work",
-  --       path = "~/vaults/work",
-  --     },
-  --   },
-  --
-  --   -- see below for full list of options 👇
-  -- },
+  opts = {
+    workspaces = {
+      {
+        name = "default",
+        path = "~/obsidian-vault",
+      },
+    },
+    completion = {
+      nvim_cmp = true,
+      min_chars = 2,
+    },
+    picker = {
+      name = "telescope.nvim",
+      mappings = {
+        -- Create a new note from your query.
+        new = "<C-x>",
+        -- Insert a link to the selected note.
+        insert_link = "<C-l>",
+      },
+    },
+
+    -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
+    -- URL it will be ignored but you can customize this behavior here.
+    ---@param url string
+    follow_url_func = function(url)
+      vim.fn.jobstart({ "xdg-open", url })
+    end,
+  },
 }
