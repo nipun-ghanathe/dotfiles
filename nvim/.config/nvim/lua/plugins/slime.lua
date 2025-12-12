41b146bc744ee7b10f1ad43b6b6edef1e511e721
nipun <nipuncod@gmail.com>
2025-12-12 16:59:32 +0530

    (nvim) configured slime to use neovim instead of kitty and also improved shortcuts

--- nvim/.config/nvim/lua/plugins/slime.lua
+++ nvim/.config/nvim/lua/plugins/slime.lua
@@ -1,20 +1,41 @@
 return {
   "jpalardy/vim-slime",
-  keys = {
-    { "<localleader>st", ft = "python" },
-  },
+  lazy = true,
   init = function()
-    vim.g.slime_target = "kitty"
-    vim.g.slime_bracketed_paste = 1
-    vim.g.slime_no_mappings = 1
-    -- vim.b.slime_cell_delimiter in respective ftplugin to override this
-    vim.g.slime_cell_delimiter = [[^#\s*%%]]
+    -- these two should be set before the plugin loads
+    vim.g.slime_target = "neovim"
+    vim.g.slime_no_mappings = true
   end,
   config = function()
+    -- options
+    vim.g.slime_bracketed_paste = true
+
+    -- options specific for target = neovim
+    vim.g.slime_input_pid = false
+    vim.g.slime_suggest_default = true
+    vim.g.slime_menu_config = false
+    vim.g.slime_neovim_ignore_unlisted = false
+
+    -- slime cell delimiter
+    vim.g.slime_cell_delimiter = [[^#\s*%%]]
+    local ft_cell_delimiters = {
+      python = [[^#\s*%%]],
+    }
+    vim.api.nvim_create_autocmd("FileType", {
+      desc = "Set slime cell delimiter",
+      pattern = "*",
+      callback = function()
+        local ft = vim.bo.filetype
+        if ft_cell_delimiters[ft] then
+          vim.b.slime_cell_delimiter = ft_cell_delimiters[ft]
+        end
+      end,
+    })
+
     -- Setting up functions
     local function slime_send_raw(to_send)
       local orig = vim.g.slime_bracketed_paste
-      vim.g.slime_bracketed_paste = 0
+      vim.g.slime_bracketed_paste = false
       vim.fn["slime#send"](to_send)
       vim.g.slime_bracketed_paste = orig
     end
@@ -22,7 +43,8 @@ return {
       slime_send_raw(vim.api.nvim_replace_termcodes("<C-" .. char .. ">", true, false, true))
     end
     local function send_cr()
-      slime_send_raw(vim.api.nvim_replace_termcodes("<CR>", true, false, true))
+      -- slime_send_raw(vim.api.nvim_replace_termcodes("<CR>", true, false, true)) -- sends \r
+      slime_send_raw("\n")
     end
     local function clear_then(action)
       send_ctrl("l")
@@ -39,37 +61,27 @@ return {
       local text = table.concat(lines, "\n")
       vim.fn["slime#send"](text)
       send_cr()
-      send_cr()
     end
 
-    -- launch terminal window keymaps
-    local slime_term_command_table = {
-      python = "uv run ipython3",
-    }
-    vim.keymap.set("n", "<localleader>st", function()
-      local slime_term_command = slime_term_command_table[vim.bo.filetype] or vim.bo.filetype
-      vim.cmd([[silent !kitty @ launch --cwd current --dont-take-focus ]] .. slime_term_command)
-    end, { buffer = true, desc = "Slime: Launch Terminal" })
-
     -- clear-then-send keymaps
-    vim.keymap.set("n", "<leader>sn", function()
+    vim.keymap.set("n", "<c-cr>", function()
       clear_then(send_cell_and_next)
     end, { desc = "Slime: Clear => Send Cell and Next" })
-    vim.keymap.set("n", "<leader>sb", function()
+    vim.keymap.set("n", "<s-cr>", function()
       clear_then(vim.fn["slime#send_cell"])
     end, { desc = "Slime: Clear => Send Cell/Block" })
-    vim.keymap.set("n", "<leader>sr", function()
+    vim.keymap.set("n", "<leader>sl", function()
       clear_then(function()
         vim.fn["slime#send_lines"](vim.v.count1)
       end)
-    end, { desc = "Slime: Clear => Send Region/Line" })
+    end, { desc = "Slime: Clear => Send Line" })
     vim.keymap.set(
       "v",
-      "<leader>sr",
+      "<c-cr>",
       [[:<c-u>call slime#send("clear\n") | call slime#send_op(visualmode(), 1)<cr>]],
       { desc = "Slime: Clear => Send Region" }
     )
-    vim.keymap.set("n", "<leader>sf", function()
+    vim.keymap.set("n", "<c-m-cr>", function()
       clear_then(send_file)
     end, { desc = "Slime: Clear => Send File" })
 
@@ -88,13 +100,13 @@ return {
     end, { desc = "Slime: Send Return (<CR>)" })
 
     -- direct-send keymaps (without clearing)
-    vim.keymap.set("n", "<leader>ssn", send_cell_and_next, { desc = "Slime: Send Cell and Next" })
-    vim.keymap.set("n", "<leader>ssb", vim.fn["slime#send_cell"], { desc = "Slime: Send Cell/Block" })
-    vim.keymap.set("n", "<leader>ssr", function()
+    vim.keymap.set("n", "<leader>sn", send_cell_and_next, { desc = "Slime: Send Cell and Next" })
+    vim.keymap.set("n", "<leader>sb", vim.fn["slime#send_cell"], { desc = "Slime: Send Cell/Block" })
+    vim.keymap.set("n", "<c-space>", function()
       vim.fn["slime#send_lines"](vim.v.count1)
     end, { desc = "Slime: Send Region/Line" })
-    vim.keymap.set("v", "<leader>ssr", ":<c-u>call slime#send_op(visualmode(), 1)<cr>", { desc = "Slime: Send Region" })
-    vim.keymap.set("n", "<leader>ssf", send_file, { desc = "Slime: Send File" })
+    vim.keymap.set("v", "<leader>sr", ":<c-u>call slime#send_op(visualmode(), 1)<cr>", { desc = "Slime: Send Region" })
+    vim.keymap.set("n", "<leader>sf", send_file, { desc = "Slime: Send File" })
     -- vim.keymap.set("n", "<leader>ssm", "<Plug>SlimeMotionSend", { desc = "Slime: Send Motion" })
     -- vim.keymap.set("n", "<leader>ssp", "<Plug>SlimeParagraphSend", { desc = "Slime: Send Paragraph" })
     -- vim.keymap.set("n", "<c-c>v", "<Plug>SlimeConfig", { desc = "Slime: Config" })
