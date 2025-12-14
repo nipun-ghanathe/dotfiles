8ac983b21657ee5d863aaefeeb004992af2bb847
nipun <nipuncod@gmail.com>
2025-12-14 21:21:44 +0530

    (nvim) removed some useless mappings

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -14,14 +14,6 @@ vim.keymap.set({ "n", "v" }, "gP", '"+P', { desc = "Paste before cursor from sys
 vim.keymap.set("n", "]q", "<cmd>cnext<cr>", { desc = "Quick-fix: Next" })
 vim.keymap.set("n", "[q", "<cmd>cprev<cr>", { desc = "Quick-fix: Previous" })
 
--- Improving scrolling experience
--- vim.keymap.set("n", "<c-e>", "<c-e>zz")
--- vim.keymap.set("n", "<c-y>", "<c-y>zz")
-vim.keymap.set("n", "<c-d>", "<c-d>zz")
-vim.keymap.set("n", "<c-u>", "<c-u>zz")
-vim.keymap.set("n", "<c-f>", "<c-f>zz")
-vim.keymap.set("n", "<c-b>", "<c-b>zz")
-
 -- Improving search scrolling experience
 vim.keymap.set("n", "n", "nzz")
 vim.keymap.set("n", "N", "Nzz")
