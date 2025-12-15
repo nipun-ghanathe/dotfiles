e3f4613d4314e53ceed30dc469343a75ab368ae0
nipun <nipuncod@gmail.com>
2025-12-15 22:30:16 +0530

    (nvim) Always enter terminal-mode when entering a terminal

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -18,3 +18,29 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.keymap.set("n", "q", "<cmd>q<cr>", { buffer = true })
   end,
 })
+
+-- Commands to run on TermOpen
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = augroup,
+  desc = "Commands to run on TermOpen",
+  callback = function()
+    if vim.o.buftype ~= "terminal" then
+      return
+    end
+
+    vim.cmd.startinsert()
+  end,
+})
+
+-- Commands to run on entering a terminal window
+vim.api.nvim_create_autocmd("WinEnter", {
+  group = augroup,
+  desc = "Commands to run on entering a terminal window",
+  callback = function()
+    if vim.o.buftype ~= "terminal" then
+      return
+    end
+
+    vim.cmd.startinsert()
+  end,
+})
