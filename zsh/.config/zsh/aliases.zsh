cbcf24638eee656f54be7fb61b2c0bfcb9e652c7
nipun <nipuncod@gmail.com>
2025-12-16 22:22:49 +0530

    batcat alias modification in aliases.zsh

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -27,7 +27,7 @@ alias lt="ls -T"
 alias tree="ls -T"
 
 # batcat
-alias cat="batcat --style plain --paging always"
+alias bat="batcat --style plain --paging always"
 
 # grep
 alias grep="grep --color=auto"
