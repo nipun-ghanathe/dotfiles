abcacca19a6a881ff196391d035f346c6517f882
nipun <nipuncod@gmail.com>
2025-12-16 18:40:01 +0530

    (nvim) escape to normal mode in terminal mode

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -8,9 +8,10 @@ vim.keymap.set("n", "<leader>e", "<cmd>Ex<cr>", { desc = "Explore - Open netrw" 
 -- Managing system clipboard
 vim.keymap.set({ "n", "v" }, "gy", '"+y', { desc = "Yank to system clipboard" })
 vim.keymap.set("n", "gp", '"+p', { desc = "Paste from system clipboard" })
-vim.keymap.set({ "n" }, "gP", '"+P', { desc = "Paste before cursor from system clipboard" })
+vim.keymap.set("n", "gP", '"+P', { desc = "Paste before cursor from system clipboard" })
 
 -- Terminal configuration
+vim.keymap.set("t", "<c-[>", vim.cmd.stopinsert)
 vim.keymap.set("n", "<leader>tt", "<cmd>$tab term<cr>", { desc = "Terminal" })
 vim.keymap.set("n", "<leader>th", function()
   vim.cmd("botright term")
