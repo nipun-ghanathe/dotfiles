f5c801d770e7a28291e531fd9c4a821d6c4641be
nipun <nipuncod@gmail.com>
2025-12-16 13:06:08 +0530

    (nvim) removed redundant code related to slime and python

--- nvim/.config/nvim/ftplugin/python.lua
+++ nvim/.config/nvim/ftplugin/python.lua
@@ -1,3 +1,2 @@
 vim.g.load_ipython = true
-vim.g.load_ipython_slime = true
 vim.g.load_venv_activate = true
--- nvim/.config/nvim/ftplugin/python/ipython_slime.lua
+++
@@ -1,8 +0,0 @@
-if not vim.g.load_ipython_slime then
-  return
-end
-
--- Set the cell delimiter
-vim.b.slime_cell_delimiter = [[^#\s\?%%]]
--- Set the REPL command
-vim.b.slime_repl_command = "uv run ipython3"
