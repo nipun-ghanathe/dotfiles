f34cdc364e1e0563c927a4c24bce8f7efb88e6a8
nipun <nipuncod@gmail.com>
2025-12-16 12:28:32 +0530

    (nvim) configured dadbod-ui

--- nvim/.config/nvim/lua/plugins/dadbod.lua
+++ nvim/.config/nvim/lua/plugins/dadbod.lua
@@ -13,5 +13,16 @@ return {
   init = function()
     -- Your DBUI configuration
     vim.g.db_ui_use_nerd_fonts = 1
+
+    vim.g.db_ui_execute_on_save = 0
+
+    vim.g.dbs = {
+      { name = "analysis-pracsql", url = "postgres://nipun@localhost/analysis" },
+      { name = "startersql-cwh", url = "mysql://nipun@localhost/startersql" },
+    }
+  end,
+  config = function()
+    vim.keymap.set("n", "<c-cr>", "<Plug>(DBUI_ExecuteQuery)")
+    vim.keymap.set("n", "<c-s-cr>", "vip<Plug>(DBUI_ExecuteQuery)")
   end,
 }
