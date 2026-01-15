d1dbb587a01375f97bc9beb35eaa41c04f918329
nipun <nipuncod@gmail.com>
2026-01-16 01:04:40 +0530

    Fix mistake in modifying highlights for lua files

--- .config/nvim/ftplugin/lua.lua
+++ .config/nvim/ftplugin/lua.lua
@@ -1,4 +1,4 @@
 -- Clearing the 'DiagnosticUnnecessary' higroup
-local ns = vim.api.nvim_create_namespace("lua_diag_unnecessary_disable")
-vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", {})
-vim.api.nvim_set_hl_ns(ns)
+local ns = vim.api.nvim_create_namespace("ft_lua_custom_hl_ns")
+vim.api.nvim_set_hl(ns, "DiagnosticUnnecessary", {})
+vim.api.nvim_win_set_hl_ns(0, ns)
