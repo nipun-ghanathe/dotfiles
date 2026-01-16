12453ff346b95f910daccfc46b3ac46bea034e4a
nipun <nipuncod@gmail.com>
2026-01-17 02:20:48 +0530

    (nvim) Remove all plugin spec related to lazy.nvim

--- .config/nvim/lua/plugins/core/autopairs.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "windwp/nvim-autopairs",
-  dependencies = { "windwp/nvim-ts-autotag", opts = {} },
-  event = "InsertEnter",
-  opts = {},
-}
--- .config/nvim/lua/plugins/core/surround.lua
+++
@@ -1,9 +0,0 @@
-vim.keymap.set("o", "ir", "i]")
-vim.keymap.set("o", "ar", "a]")
-
-return {
-  "kylechui/nvim-surround",
-  version = "*",
-  event = { "BufReadPost", "BufNewFile" },
-  opts = {},
-}
