61c518d8c12afa6ef66076632971038bbfac292b
nipun <nipuncod@gmail.com>
2026-01-17 20:59:00 +0530

    (nvim) Fix a minor issue with vim-fugitive

--- .config/nvim/lua/config/git.lua
+++ .config/nvim/lua/config/git.lua
@@ -10,8 +10,10 @@ vim.api.nvim_create_autocmd("User", {
   group = "user_autocmds",
   desc = "configure fugitive summary window",
   pattern = { "FugitiveIndex" },
-  callback = function()
-    vim.api.nvim_win_set_cursor(0, { 5, 0 })
+  callback = function(ev)
+    if vim.api.nvim_buf_line_count(ev.buf) >= 5 then
+      vim.api.nvim_win_set_cursor(0, { 5, 0 })
+    end
     vim.opt_local.cursorlineopt = "both"
     vim.keymap.set("n", "q", "gq", { desc = "Close fugitive summary window", buffer = true, remap = true })
   end,
