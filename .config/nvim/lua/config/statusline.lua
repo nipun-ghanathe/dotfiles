be8da0011c9fb8c21728009c3301dd3b33196222
nipun <nipuncod@gmail.com>
2026-01-19 20:17:15 +0530

    (nvim) Update lualine to include more info about file

--- .config/nvim/lua/config/statusline.lua
+++ .config/nvim/lua/config/statusline.lua
@@ -2,7 +2,7 @@ vim.pack.add({ "https://github.com/nvim-lualine/lualine.nvim" })
 
 require("lualine").setup({
   sections = {
-    lualine_c = { "%f" },
+    lualine_c = { "%f %h%w%m%r" },
   },
   options = {
     component_separators = "|",
