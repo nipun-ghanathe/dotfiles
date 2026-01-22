cb1a99904e6bdc2b7d9c4ad5d79f848660d2b869
nipun <nipuncod@gmail.com>
2026-01-22 21:08:41 +0530

    (nvim) Move netrw ftplugin config to ft autocmd in config.netrw

--- .config/nvim/lua/config/netrw.lua
+++ .config/nvim/lua/config/netrw.lua
@@ -8,3 +8,17 @@ vim.api.nvim_set_hl(0, "netrwMarkFile", { link = "Search" })
 -- Mappings
 vim.keymap.set("n", "<leader>e", "<cmd>Ex<cr>", { desc = "Explore - Open netrw" })
 vim.keymap.set("n", "<leader>.", "<cmd>e .<cr>", { desc = "Explore - Open netrw in cwd" })
+
+-- options and mappings inside netrw
+vim.api.nvim_create_autocmd("FileType", {
+  group = "user_autocmds",
+  desc = "options and mappings for netrw ft",
+  pattern = "netrw",
+  callback = function()
+    -- Options
+    vim.opt_local.cursorlineopt = "both"
+    -- Mappings
+    vim.keymap.set("n", "h", "-", { buffer = true, remap = true }) -- go up a directory
+    vim.keymap.set("n", "l", "<cr>", { buffer = true, remap = true }) -- open directory/file under cursor
+  end,
+})
