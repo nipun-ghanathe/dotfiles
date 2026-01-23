fe7d650a8325964f04f5412ae57524e57f80d42c
nipun <nipuncod@gmail.com>
2026-01-23 13:45:48 +0530

    (nvim) Improve PackDel usercmd

--- .config/nvim/lua/config/pack.lua
+++ .config/nvim/lua/config/pack.lua
@@ -4,11 +4,16 @@ vim.api.nvim_create_augroup("pack_user_autocmds", { clear = true })
 -- helper functions
 
 local function get_pkg_names()
-  local pkg_names = {}
-  for _, pkg in ipairs(vim.pack.get()) do
-    table.insert(pkg_names, pkg.spec.name)
-  end
-  return pkg_names
+  return vim.iter(vim.pack.get())
+      :map(function(x) return x.spec.name end)
+      :totable()
+end
+
+local function get_inactive_pkg_names()
+  return vim.iter(vim.pack.get())
+      :filter(function(x) return not x.active end)
+      :map(function(x) return x.spec.name end)
+      :totable()
 end
 
 -- usercmds
@@ -23,5 +28,7 @@ vim.api.nvim_create_user_command("PackUpdate", function(opts)
 end, { desc = "Update Packages", nargs = "*", complete = get_pkg_names })
 
 vim.api.nvim_create_user_command("PackDel", function(opts)
-  vim.pack.del(opts.fargs, { force = opts.bang })
-end, { desc = "Delete Packages (! = force)", nargs = "+", bang = true, complete = get_pkg_names })
+  local pkgs = not vim.tbl_isempty(opts.fargs)
+      and opts.fargs or get_inactive_pkg_names()
+  vim.pack.del(pkgs, { force = opts.bang })
+end, { desc = "Delete Packages (! = force)", nargs = "*", bang = true, complete = get_pkg_names })
