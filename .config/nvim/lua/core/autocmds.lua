954910ac779f08c736ae9f2212a920dda2142f27
nipun <nipuncod@gmail.com>
2026-01-26 10:57:23 +0530

    (nvim) No quitting using q in man, help, git buffers

--- .config/nvim/lua/core/autocmds.lua
+++ .config/nvim/lua/core/autocmds.lua
@@ -8,13 +8,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.hl.on_yank({ higroup = "Visual", timeout = 200 })
   end,
 })
-
--- Exit using 'q' on some filetypes
-vim.api.nvim_create_autocmd("FileType", {
-  group = "user_autocmds",
-  desc = "Quit some windows simply using 'q'",
-  pattern = { "help", "man", "git" },
-  callback = function()
-    vim.keymap.set("n", "q", "<cmd>q<cr>", { buffer = true })
-  end,
-})
