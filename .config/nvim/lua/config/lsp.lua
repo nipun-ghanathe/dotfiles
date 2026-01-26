7a23177302efaafba8304cdf4da12ab55934f9ff
nipun <nipuncod@gmail.com>
2026-01-26 10:47:16 +0530

    (nvim) move luals format and nvim config to vim.lsp.config instead of luarc and .editorconfig

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -8,6 +8,64 @@ vim.pack.add({ "https://github.com/neovim/nvim-lspconfig" })
 vim.api.nvim_create_augroup("lsp_user_autocmds", { clear = true })
 
 -- Configure LSPs
+vim.lsp.config("lua_ls", {
+  settings = {
+    Lua = {
+      format = {
+        enable = true,
+        defaultConfig = {
+          indent_style = "space",
+          indent_size = "2",
+          quote_style = "double",
+
+          trailing_table_separator = "smart",
+          call_arg_parentheses = "always",
+
+          max_line_length = "120",
+          insert_final_newline = "true",
+          space_before_inline_comment = "1",
+          break_all_list_when_line_exceed = "true",
+
+          align_call_args = "false",
+          align_function_params = "false",
+          align_continuous_assign_statement = "false",
+          align_continuous_rect_table_field = "false",
+          align_continuous_line_space = "false",
+          align_if_branch = "false",
+          align_array_table = "false",
+          align_continuous_similar_call_args = "false",
+          align_continuous_inline_comment = "false",
+        },
+      },
+    },
+  },
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if
+          path ~= vim.fn.stdpath("config")
+          and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+      then
+        return
+      end
+    end
+    client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+      runtime = {
+        version = "LuaJIT",
+        path = {
+          "lua/?.lua",
+          "lua/?/init.lua",
+        },
+      },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+        },
+      },
+    })
+  end,
+})
 vim.lsp.config("ruff", {
   on_attach = function()
     vim.api.nvim_create_autocmd("BufWritePre", {
