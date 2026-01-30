5342b8c90b6bb84a8f903ad05146f62bad862244
nipun <nipuncod@gmail.com>
2026-01-30 18:58:05 +0530

    rclone: add ~/projects to sync

--- .local/bin/sync-gdrive.sh
+++ .local/bin/sync-gdrive.sh
@@ -8,6 +8,14 @@ rclone sync /home/nipun/code gdrive:backup/code \
   --exclude '**/.ipynb_checkpoints/**' \
   --exclude '**/.pytest_cache/**'
 
+rclone sync /home/nipun/projects gdrive:backup/projects \
+  --verbose \
+  --progress \
+  --exclude '**/.venv/**' \
+  --exclude '**/__pycache__/**' \
+  --exclude '**/.ipynb_checkpoints/**' \
+  --exclude '**/.pytest_cache/**'
+
 rclone sync /home/nipun/obsidian-vault gdrive:backup/obsidian-vault \
   --verbose \
   --progress
