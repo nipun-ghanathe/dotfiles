a1f47452b98659ac02e28fd3c4007bef3b21ebe3
nipun <nipuncod@gmail.com>
2026-01-30 21:47:09 +0530

    nvim: override term statusbar hlgroups for rose-pine

--- .config/nvim/lua/config/colors.lua
+++ .config/nvim/lua/config/colors.lua
@@ -1,5 +1,11 @@
 vim.pack.add({
   { src = "https://github.com/rose-pine/neovim", name = "rose-pine" },
 })
-require("rose-pine").setup({ variant = "moon" })
+require("rose-pine").setup({
+  variant = "moon",
+  highlight_groups = {
+    StatusLineTerm = { link = "StatusLine" },
+    StatusLineTermNC = { link = "StatusLineNC" },
+  },
+})
 vim.cmd.colorscheme("rose-pine")
