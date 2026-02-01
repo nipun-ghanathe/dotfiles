263046728d1666b4d965a962fc407260b9ffc2a3
nipun <nipuncod@gmail.com>
2026-02-01 13:19:05 +0530

    nvim: use right terminology for arglist keymaps

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -5,13 +5,13 @@ vim.g.maplocalleader = "\\"
 -- Navigating files using arglist
 vim.keymap.set("n", "<leader>a", function()
   vim.cmd("$argadd")
-  vim.notify("Buffer added to arglist")
-end, { desc = "Add current buffer to arglist " })
+  vim.notify("File added to arglist")
+end, { desc = "Add current file to arglist " })
 vim.keymap.set("n", "<leader>A", require("config.arglist").edit_arglist, { desc = "Edit Arglist" })
-vim.keymap.set("n", "<m-j>", "<cmd>argument 1<cr>", { desc = "Go to first buffer in arglist" })
-vim.keymap.set("n", "<m-k>", "<cmd>argument 2<cr>", { desc = "Go to first buffer in arglist" })
-vim.keymap.set("n", "<m-l>", "<cmd>argument 3<cr>", { desc = "Go to first buffer in arglist" })
-vim.keymap.set("n", "<m-;>", "<cmd>argument 4<cr>", { desc = "Go to first buffer in arglist" })
+vim.keymap.set("n", "<m-j>", "<cmd>argument 1<cr>", { desc = "Go to first file in arglist" })
+vim.keymap.set("n", "<m-k>", "<cmd>argument 2<cr>", { desc = "Go to second file in arglist" })
+vim.keymap.set("n", "<m-l>", "<cmd>argument 3<cr>", { desc = "Go to third file in arglist" })
+vim.keymap.set("n", "<m-;>", "<cmd>argument 4<cr>", { desc = "Go to fourth file in arglist" })
 
 -- Managing system clipboard
 vim.keymap.set({ "n", "v" }, "gy", '"+y', { desc = "Yank to system clipboard" })
