aae887869e348ac8f0cff61b20dc1bf89f22f8f7
nipun <nipuncod@gmail.com>
2026-02-02 15:52:45 +0530

    nvim: don't customize netrw a lot

--- .config/nvim/lua/config/netrw.lua
+++ .config/nvim/lua/config/netrw.lua
@@ -1,9 +1,7 @@
 -- Options
 -- vim.g.netrw_liststyle = 3 -- tree list style
 vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 30
 vim.g.netrw_localcopydircmd = "cp -r"
-vim.api.nvim_set_hl(0, "netrwMarkFile", { link = "Search" })
 
 -- Mappings
 vim.keymap.set("n", "<leader>e", "<cmd>Ex<cr>", { desc = "Explore - Open netrw" })
