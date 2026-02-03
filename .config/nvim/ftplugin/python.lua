d566cff6d0314050ff1e36a385eba8ce6777b746
nipun <nipuncod@gmail.com>
2026-02-03 10:23:18 +0530

    nvim: refactor ftplugin/python

--- .config/nvim/ftplugin/python.lua
+++ .config/nvim/ftplugin/python.lua
@@ -1,1 +1,11 @@
-vim.g.load_venv_activate = true
+local client = vim.lsp.get_clients({ name = "pyright" })[1]
+local root_dir = client and client.root_dir or vim.fn.getcwd()
+
+-- activate venv if exists and not activated
+if not vim.g.python_venv_activated and vim.uv.fs_stat(root_dir .. "/.venv/bin/python") then
+  vim.g.python_venv_activated = true
+  if not string.find(vim.env.PATH, root_dir .. "/.venv/bin") then
+    vim.env.PATH = root_dir .. "/.venv/bin" .. ":" .. vim.env.PATH
+  end
+  vim.env.VIRTUAL_ENV = root_dir .. "/.venv"
+end
