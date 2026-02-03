47050a4fd95eda69adea2c38c4e1d4c9e0a6c1fb
nipun <nipuncod@gmail.com>
2026-02-03 08:36:23 +0530

    nvim: don't alter timeoutlen

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -16,6 +16,5 @@ vim.o.hlsearch = false
 vim.o.foldenable = false
 vim.o.exrc = true
 vim.o.confirm = true
-vim.o.timeoutlen = 500 -- Decreasing mapped sequence wait time
 vim.o.swapfile = false -- Disable swapfile creation
 vim.o.undofile = true -- Setting persistent undo
