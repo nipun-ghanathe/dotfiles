00a60a3fb8390fb6e6bf0c14dc56a50094507b12
nipun <nipuncod@gmail.com>
2026-02-03 09:06:27 +0530

    nvim: remove ftplugin/lua

--- .config/nvim/ftplugin/lua.lua
+++
@@ -1,4 +0,0 @@
--- Clearing the 'DiagnosticUnnecessary' higroup
-local ns = vim.api.nvim_create_namespace("ft_lua_custom_hl_ns")
-vim.api.nvim_set_hl(ns, "DiagnosticUnnecessary", {})
-vim.api.nvim_win_set_hl_ns(0, ns)
