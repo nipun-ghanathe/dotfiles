f18d497bbc2d958998e80cd307ea49f38cf8e492
nipun <nipuncod@gmail.com>
2026-02-03 09:03:53 +0530

    nvim: do not do useless lsp configuration

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -4,16 +4,6 @@ vim.pack.add({ "https://github.com/neovim/nvim-lspconfig" })
 --- Setting up LSP Servers ---
 ------------------------------
 
--- Create augroup to use with lsp related autocmds
-vim.api.nvim_create_augroup("lsp_user_autocmds", { clear = true })
-
--- Configure LSPs
--- TODO: If Neovim gets snippet support remove these lines
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-vim.lsp.config("html", { capabilities = capabilities })
-vim.lsp.config("cssls", { capabilities = capabilities })
-
 -- Enable LSPs
 vim.lsp.enable({
   "lua_ls",
@@ -43,9 +33,12 @@ vim.diagnostic.config({
 --- LSP mappings and usercmds ---
 ---------------------------------
 
+-- Create augroup to use with lsp related autocmds
+vim.api.nvim_create_augroup("lsp_user_autocmds", { clear = true })
+
 vim.api.nvim_create_autocmd("LspAttach", {
   group = "lsp_user_autocmds",
-  desc = "Set LSP related keymaps",
+  desc = "Set LSP related keymaps and usercmds",
   callback = function(ev)
     -- Close completion menu if visible and view signature help
     vim.keymap.set({ "i", "s" }, "<c-h>", function()
