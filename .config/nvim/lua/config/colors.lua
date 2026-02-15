48e1113b02b03b92e24077688fd9454a59393cac
nipun <nipuncod@gmail.com>
2026-02-15 11:48:54 +0530

    nvim: no bg_gutter

--- .config/nvim/lua/config/colors.lua
+++ .config/nvim/lua/config/colors.lua
@@ -1,3 +1,14 @@
 vim.pack.add({ "https://github.com/rebelot/kanagawa.nvim" })
-require("kanagawa").setup({ compile = true })
+require("kanagawa").setup({
+  compile = true,
+  colors = {
+    theme = {
+      all = {
+        ui = {
+          bg_gutter = "none",
+        },
+      },
+    },
+  },
+})
 vim.cmd.colorscheme("kanagawa")
