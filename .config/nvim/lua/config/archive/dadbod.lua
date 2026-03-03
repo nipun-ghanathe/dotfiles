adcb5a99f1172c40ac765f7648fe70c97e7c1e59
nipun <nipuncod@gmail.com>
2026-03-03 19:12:04 +0530

    nvim: add ccc.nvim (in archive)

--- .config/nvim/lua/config/archive/dadbod.lua
+++ .config/nvim/lua/config/archive/dadbod.lua
@@ -2,7 +2,6 @@ vim.pack.add({
   "https://github.com/kristijanhusak/vim-dadbod-ui",
   "https://github.com/tpope/vim-dadbod",
   "https://github.com/kristijanhusak/vim-dadbod-completion",
-  "https://github.com/hrsh7th/nvim-cmp",
 })
 
 -- Configuration
