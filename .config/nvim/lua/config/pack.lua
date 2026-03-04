72521ca48c7b2265054c6b76147e0438253ddf07
nipun <nipuncod@gmail.com>
2026-03-04 22:32:08 +0530

    nvim: improve pack_usercmds complete functions

--- .config/nvim/lua/config/pack.lua
+++ .config/nvim/lua/config/pack.lua
@@ -3,16 +3,28 @@ vim.api.nvim_create_augroup("pack_user_autocmds", { clear = true })
 
 -- helper functions
 
-local function get_pkg_names()
+local function get_pkg_names(starts_with)
   return vim.iter(vim.pack.get())
       :map(function(x) return x.spec.name end)
+      :filter(function(name)
+        if not starts_with or starts_with == "" then
+          return true
+        end
+        return string.sub(name, 1, #starts_with) == starts_with
+      end)
       :totable()
 end
 
-local function get_inactive_pkg_names()
+local function get_inactive_pkg_names(starts_with)
   return vim.iter(vim.pack.get())
       :filter(function(x) return not x.active end)
       :map(function(x) return x.spec.name end)
+      :filter(function(name)
+        if not starts_with or starts_with == "" then
+          return true
+        end
+        return string.sub(name, 1, #starts_with) == starts_with
+      end)
       :totable()
 end
 
