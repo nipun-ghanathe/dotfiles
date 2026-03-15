ed67f96b6e5159a56c9dec57f997e49a4f6b3d99
nipun <nipuncod@gmail.com>
2026-03-15 15:57:54 +0530

    nvim: remove useless config.extras

--- .config/nvim/lua/config/extras/nvim-emmet.lua
+++
@@ -1,8 +0,0 @@
-vim.pack.add({ "https://github.com/olrtg/nvim-emmet" })
-
-vim.keymap.set(
-  { "n", "v" },
-  "<leader>xe",
-  require("nvim-emmet").wrap_with_abbreviation,
-  { desc = "Wrap with Emmet Abbreviation" }
-)
