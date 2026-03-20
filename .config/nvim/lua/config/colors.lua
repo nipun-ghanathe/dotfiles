7b6aec35ed071228b7b3140f66c9361f9f595f70
nipun <nipuncod@gmail.com>
2026-03-20 19:12:43 +0530

    nvim: kanagawa - no undercurls and bg_gutter

--- .config/nvim/lua/config/colors.lua
+++ .config/nvim/lua/config/colors.lua
@@ -1,3 +1,17 @@
 vim.pack.add({ 'https://github.com/rebelot/kanagawa.nvim' })
-require('kanagawa').setup({ compile = true })
+
+require('kanagawa').setup({
+  compile = true,
+  undercurl = false,
+  colors = {
+    theme = {
+      all = {
+        ui = {
+          bg_gutter = 'none',
+        },
+      },
+    },
+  },
+})
+
 vim.cmd.colorscheme('kanagawa')
