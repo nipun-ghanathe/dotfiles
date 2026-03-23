1fe01b269e69c1b1ed598c1a2819bea35be1ae13
nipun <nipuncod@gmail.com>
2026-03-23 10:56:56 +0530

    nvim: use gruvbox-material plugin

--- .config/nvim/lua/config/colors.lua
+++ .config/nvim/lua/config/colors.lua
@@ -1,16 +1,3 @@
-vim.pack.add({ 'https://github.com/ellisonleao/gruvbox.nvim' })
-
-require('gruvbox').setup({
-  bold = false,
-  italic = { strings = false },
-  overrides = {
-    SignColumn = { bg = 'NONE' },
-    DiagnosticSignError = { link = 'DiagnosticError' },
-    DiagnosticSignWarn = { link = 'DiagnosticWarn' },
-    DiagnosticSignInfo = { link = 'DiagnosticInfo' },
-    DiagnosticSignHint = { link = 'DiagnosticHint' },
-    DiagnosticSignOk = { link = 'DiagnosticOk' },
-  },
-})
-
-vim.cmd.colorscheme('gruvbox')
+vim.pack.add({ 'https://github.com/sainnhe/gruvbox-material' })
+vim.g.gruvbox_material_foreground = 'original'
+vim.cmd.colorscheme('gruvbox-material')
