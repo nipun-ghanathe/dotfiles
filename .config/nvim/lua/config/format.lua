cd60a7bd6ccf90377353bdabcb25eacc9011e2b3
nipun <nipuncod@gmail.com>
2026-03-24 14:21:16 +0530

    nvim: simplify config.format

--- .config/nvim/lua/config/format.lua
+++ .config/nvim/lua/config/format.lua
@@ -3,63 +3,41 @@ vim.pack.add({ 'https://github.com/stevearc/conform.nvim' })
 require('conform').setup({
   formatters = {
     prettier = {
-      append_args = { '--config-precedence=prefer-file', '--no-semi', '--single-quote' }
-    }
+      append_args = { '--config-precedence=prefer-file', '--no-semi', '--single-quote' },
+    },
   },
   formatters_by_ft = {
+    python = { 'ruff_format', 'ruff_organize_imports' },
     javascript = { 'prettier' },
     typescript = { 'prettier' },
     javascriptreact = { 'prettier' },
     typescriptreact = { 'prettier' },
   },
+  default_format_opts = { timeout_ms = 2000, lsp_format = 'fallback' },
+  format_on_save = function(bufnr)
+    if
+      vim.b[bufnr].formatonsave ~= true
+      and (vim.b.formatonsave ~= nil or not vim.g.formatonsave)
+    then
+      return
+    end
+    return { timeout_ms = 500, lsp_format = 'fallback' }
+  end,
 })
 
-local organize_imports_filetypes = { 'python' }
-
-local function format_file(bufnr)
-  bufnr = bufnr or 0
-  local conform_opts = { bufnr = bufnr, lsp_format = 'fallback', timeout_ms = 2000 }
-  require('conform').format(conform_opts)
-
-  if vim.list_contains(organize_imports_filetypes, vim.bo[bufnr].filetype) then
-    vim.lsp.buf.code_action({
-      ---@diagnostic disable-next-line: missing-fields
-      context = { only = { 'source.organizeImports' } },
-      apply = true,
-    })
-  end
-end
+-- usercmd to toggle format_on_save
+vim.api.nvim_create_user_command('FormatOnSaveToggle', function(opts)
+  local var = opts.bang and vim.b or vim.g
+  var.formatonsave = var.formatonsave ~= true
+  vim.notify(
+    string.format(
+      'format on save -> global: %s | buffer: %s',
+      vim.g.formatonsave,
+      vim.b.formatonsave == nil and 'inherit' or vim.b.formatonsave
+    ),
+    vim.log.levels.INFO
+  )
+end, { bang = true })
 
 -- keymap to format
-vim.keymap.set({ 'n', 'v' }, '<leader>gw', format_file, { desc = 'Conform: Format File' })
-
--- -----------------------------
--- --- set up format on save ---
--- -----------------------------
---
--- vim.g.formatonsave = false
---
--- vim.api.nvim_create_user_command('FormatOnSaveToggle', function(opts)
---   local var = opts.bang and vim.b or vim.g
---   var.formatonsave = var.formatonsave ~= true
---   vim.notify(
---     string.format(
---       'format on save -> global: %s | buffer: %s',
---       vim.g.formatonsave,
---       vim.b.formatonsave == nil and 'inherit' or vim.b.formatonsave
---     ),
---     vim.log.levels.INFO
---   )
--- end, { bang = true })
---
--- local augroup = vim.api.nvim_create_augroup('FormatConfig', { clear = true })
---
--- vim.api.nvim_create_autocmd('BufWritePre', {
---   group = augroup,
---   desc = 'Format before save',
---   callback = function(ev)
---     if vim.b.formatonsave == true or (vim.b.formatonsave == nil and vim.g.formatonsave) then
---       format_file(ev.buf)
---     end
---   end,
--- })
+vim.keymap.set({ 'n', 'v' }, '<leader>gw', require('conform').format, { desc = 'Conform: Format File' })
