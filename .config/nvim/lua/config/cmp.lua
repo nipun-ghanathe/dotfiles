302e2c42d8711ee37fad5f16694603040e8ad0b8
nipun <nipuncod@gmail.com>
2026-03-30 21:02:51 +0530

    nvim: remove redundant lsp cmp sorting logic

--- .config/nvim/lua/config/cmp.lua
+++ .config/nvim/lua/config/cmp.lua
@@ -39,10 +39,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
 -- Enable LSP Completion (snippets expansion, text edits, exeucte associated commands etc)
 -- TODO: If newer versions of nvim make this automatic, remove this block
 -- check `:h lsp-completion` to know
-
--- LSP clients whose completions items should be sorted before others
-local priority_lsp_clients = { 'emmet_language_server' }
-
 vim.api.nvim_create_autocmd('LspAttach', {
   group = 'lsp_user_autocmds',
   desc = 'enable lsp completion (expand snippets, text edits, exectue associated commands etc)',
@@ -51,22 +47,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
     if not client or not client:supports_method(vim.lsp.protocol.Methods.textDocument_completions) then
       return
     end
-    vim.lsp.completion.enable(true, client.id, ev.buf, {
-      cmp = function(a, b)
-        local item_a = a.user_data.nvim.lsp.completion_item
-        local item_b = b.user_data.nvim.lsp.completion_item
-
-        local client_a = a.user_data.nvim.lsp.client_name
-        local client_b = b.user_data.nvim.lsp.client_name
-
-        local is_prio_a = vim.list_contains(priority_lsp_clients, client_a)
-        local is_prio_b = vim.list_contains(priority_lsp_clients, client_b)
-        if is_prio_a ~= is_prio_b then
-          return is_prio_a
-        end
-
-        return (item_a.sortText or item_a.label) < (item_b.sortText or item_b.label)
-      end,
-    })
+    vim.lsp.completion.enable(true, client.id, ev.buf)
   end,
 })
