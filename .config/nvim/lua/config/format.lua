f4d328b8be604d52899b556717436f0ddf0e2315
nipun <nipuncod@gmail.com>
2026-04-01 13:26:02 +0530

    nvim: comment out config for using formatonsave

--- .config/nvim/lua/config/format.lua
+++ .config/nvim/lua/config/format.lua
@@ -12,30 +12,30 @@ require('conform').setup({
     typescript = { 'prettier' },
   },
   default_format_opts = { timeout_ms = 2000, lsp_format = 'fallback' },
-  format_on_save = function(bufnr)
-    if
-      vim.b[bufnr].formatonsave ~= true
-      and (vim.b.formatonsave ~= nil or not vim.g.formatonsave)
-    then
-      return
-    end
-    return { timeout_ms = 500, lsp_format = 'fallback' }
-  end,
+  -- format_on_save = function(bufnr)
+  --   if
+  --     vim.b[bufnr].formatonsave ~= true
+  --     and (vim.b.formatonsave ~= nil or not vim.g.formatonsave)
+  --   then
+  --     return
+  --   end
+  --   return { timeout_ms = 500, lsp_format = 'fallback' }
+  -- end,
 })
 
--- usercmd to toggle format_on_save
-vim.api.nvim_create_user_command('FormatOnSaveToggle', function(opts)
-  local var = opts.bang and vim.b or vim.g
-  var.formatonsave = var.formatonsave ~= true
-  vim.notify(
-    string.format(
-      'format on save -> global: %s | buffer: %s',
-      vim.g.formatonsave,
-      vim.b.formatonsave == nil and 'inherit' or vim.b.formatonsave
-    ),
-    vim.log.levels.INFO
-  )
-end, { bang = true })
+-- -- usercmd to toggle format_on_save
+-- vim.api.nvim_create_user_command('FormatOnSaveToggle', function(opts)
+--   local var = opts.bang and vim.b or vim.g
+--   var.formatonsave = var.formatonsave ~= true
+--   vim.notify(
+--     string.format(
+--       'format on save -> global: %s | buffer: %s',
+--       vim.g.formatonsave,
+--       vim.b.formatonsave == nil and 'inherit' or vim.b.formatonsave
+--     ),
+--     vim.log.levels.INFO
+--   )
+-- end, { bang = true })
 
 -- keymap to format
 vim.keymap.set({ 'n', 'v' }, '<leader>gw', require('conform').format, { desc = 'Conform: Format File' })
