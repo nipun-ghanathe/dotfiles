c57c04fa5f07fb2cd5c04c2c6446925801b56f5e
nipun <nipuncod@gmail.com>
2026-04-01 12:55:31 +0530

    nvim: don't use treesitter for folding in neovim

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -45,7 +45,7 @@ vim.api.nvim_create_autocmd('FileType', {
   callback = function()
     vim.treesitter.start()
     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-    vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-    vim.wo[0][0].foldmethod = 'expr'
+    -- vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    -- vim.wo[0][0].foldmethod = 'expr'
   end,
 })
