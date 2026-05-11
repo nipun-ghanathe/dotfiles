1b778278a4d527d804167e5900b8a2df58f66fa1
nipun <nipuncod@gmail.com>
2026-05-12 05:15:16 +0530

    hypr: remove useless lines

--- .config/hypr/modules/appearance.lua
+++ .config/hypr/modules/appearance.lua
@@ -50,16 +50,10 @@ hl.config({
 
     shadow = {
       enabled = true,
-      range = 4,
-      render_power = 3,
-      color = 0xee1a1a1a,
     },
 
     blur = {
       enabled = true,
-      size = 3,
-      passes = 1,
-      vibrancy = 0.1696,
     },
   },
 
